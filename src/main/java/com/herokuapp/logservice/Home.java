2b771a30c0ca4a2a80612f7841698e0ab5bc2555
Alessandro Giordano <alessandro.giordano2000@gmail.com>
2018-07-19 11:45:38 +0200

    1145

--- src/main/java/com/herokuapp/logservice/Home.java
+++ src/main/java/com/herokuapp/logservice/Home.java
@@ -2,6 +2,9 @@ package com.herokuapp.logservice;
 
 
 import com.assetx.libraries.utils.SqlUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
@@ -10,19 +13,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 
 @WebServlet(
         name = "HomeServlet",
-        urlPatterns = {"/"}
+        urlPatterns = {"/log/*"}
 )
 public class Home extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         //Read logs
+        OutputStream out = resp.getOutputStream();
         try {
             //Mi connetto al db
             Connection connection;
@@ -32,16 +38,20 @@ public class Home extends HttpServlet {
             Statement statement;
             String query;
 
-            //query = "SELECT email,password,attivo FROM assetmaxusers";
-
+            query = "SELECT * FROM logs";
 
             try{
                 statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(query);
 
                 while (resultSet.next()){
-
+                    String output = "";
+                    output += resultSet.getString("time");
+                    output += "\t";
+                    output += resultSet.getString("value");
+                    out.write(output.getBytes());
                 }
+                out.close();
                 connection.close();
 
 
@@ -64,6 +74,26 @@ public class Home extends HttpServlet {
         {
             response.append(new String(buffer));
         }
+        JSONObject jsonObject = new JSONObject(response);
+        //Mi connetto al db
 
+        try {
+            JSONArray logs = jsonObject.getJSONArray("logs");
+            for (int i = 0; i < logs.length(); i++){
+                JSONObject log = logs.getJSONObject(i);
+                //Salvo nel db
+                Connection connection;
+                connection = SqlUtils.getConnectionHeroku();
+                HashMap<String, String> datas = new HashMap<String, String>();
+                datas.put("time", log.getString("time"));
+                datas.put("value", log.getString("value"));
+                SqlUtils.sqlAdd(connection, datas, "logs");
+                connection.close();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
